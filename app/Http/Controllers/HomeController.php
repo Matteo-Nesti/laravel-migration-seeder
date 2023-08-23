cce4eac1204171cefa1d70dd54964f80b6912070
Matteo-Nesti <matteo.nesti@hotmail.com>
2023-08-23 16:49:15 +0200

    create model for db and print in page trains info

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,12 +2,14 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\train;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
     public function index()
     {
-        return view('welcome');
+        $trains = Train::all();
+        return view('welcome', compact('trains'));
     }
 }
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,5 +3,41 @@
 @section('title', 'Home')
 
 @section('main')
-    <h1>prova</h1>
+    <ul>
+        @foreach ($trains as $train)
+            <li>
+                {{ $train->agency_name }}
+            </li>
+            <li>
+                {{ $train->departure_station }}
+            </li>
+            <li>
+                {{ $train->arrival_station }}
+            </li>
+            <li>
+                {{ $train->departure_time }}
+            </li>
+            <li>
+                {{ $train->arrival_time }}
+            </li>
+            <li>
+                {{ $train->train_code }}
+            </li>
+            <li>
+                {{ $train->number_of_cabs }}
+            </li>
+            <li>
+                @if ($train->in_time)
+                    il treno e` in orario
+                @endif
+            </li>
+            <li>
+                @if (!$train->cancelled)
+                    il treno e` in transito
+                @else
+                    il treno e` stato soppresso
+                @endif
+            </li>
+        @endforeach
+    </ul>
 @endsection
